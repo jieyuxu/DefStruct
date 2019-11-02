82dd742af0d56da467fa6f5e8d96621c13028639
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2019-11-02 02:23:22 -0400

    update app.py

--- app.py
+++ app.py
@@ -8,18 +8,17 @@
 from sys import argv, exit
 # from database import Database
 from flask import Flask, request, make_response, redirect, url_for, render_template
-from flask_cas import CAS, login, logout, login_required
+# from flask_cas import CAS, login, logout, login_required
 #-----------------------------------------------------------------------
 
 app = Flask(__name__)
-cas = CAS(app)
-app.config['CAS_SERVER'] = "https://fed.princeton.edu/cas/login"
-app.config['CAS_AFTER_LOGIN'] = 'library'
-app.config['CAS_LOGIN_ROUTE'] = '/cas'
+# cas = CAS(app)
+# app.config['CAS_SERVER'] = "https://fed.princeton.edu/cas/login"
+# app.config['CAS_AFTER_LOGIN'] = 'library'
+# app.config['CAS_LOGIN_ROUTE'] = '/cas'
 #-----------------------------------------------------------------------
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
 #-----------------------------------------------------------------------
@@ -27,8 +26,6 @@ def index():
 @app.route('/profile')
 @login_required
 def library():
-    username = cas.username,
-    display_name = cas.attributes['cas:displayName']
     print(username)
     print(display_name)
     return render_template("mytemplates.html")
